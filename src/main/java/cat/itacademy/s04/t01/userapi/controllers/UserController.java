0820d85d20310f18716ea5c5b83b69fc1efa1a66
Spook242 <spook242@gmail.com>
2025-12-28 22:06:30 +0100

    feat: add endpoint to list all users

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,8 +1,10 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,5 +14,10 @@ import java.util.List;
 public class UserController {
 
     private static final List<User> users = new ArrayList<>();
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
 
 }
