61ce75aef080a5fdc4ff3dd561c3db66e4c40022
Spook242 <spook242@gmail.com>
2025-12-28 16:14:51 +0100

    refactor: change health endpoint response to JSON format

--- src/main/java/cat/itacademy/s04/t01/userapi/UserApiApplication.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/UserApiApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApiApplication.class, args);
+
+        SpringApplication.run(UserApiApplication.class, args);
 	}
 
 }
